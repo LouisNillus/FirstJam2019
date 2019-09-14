df4411a05d6da2e3df704ade107e0a641ff1abfb
Nicolas Capelier <capelier.nicolas@gmail.com>
2019-09-14 04:42:14 +0200

    correction scripts + liste prefabs

---
+++ Assets/Scripts/NCO_Scripts/Car_list.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Car_list : MonoBehaviour
+{
+
+    public List<GameObject> CarList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject fooObj in GameObject.FindGameObjectsWithTag("Car"))
+        {
+            CarList.Add(fooObj);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NCO_Scripts/Car_spawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Car_spawn : MonoBehaviour
+{
+
+    int delay;
+    Transform objTransform;
+
+    public List<GameObject> activeCarList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        objTransform = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        delay = Random.Range(3, 5);
+        ranCar = 
+    }
+    
+    IEnumerator SpawnCar()
+    {
+
+        activeCarList.Add(new carList[ranCar].instan);
+
+        yield return new WaitForSeconds(delay);
+        
+    }*/
+
+}
