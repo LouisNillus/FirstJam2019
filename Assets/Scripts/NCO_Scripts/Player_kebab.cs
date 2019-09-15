a6d46993af55486ece9b327b6fe24f55047fc337
Nicolas Capelier <capelier.nicolas@gmail.com>
2019-09-15 11:04:48 +0200

    kebak start + periph fix

---
+++ Assets/Scripts/NCO_Scripts/Player_kebab.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_kebab : MonoBehaviour
+{
+    bool canClick = false;
+    bool hasMoney = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Joystick1Button0) && (canClick == true) && (hasMoney == false))
+        {
+            // Instantiate();
+            hasMoney = true;
+            Debug.Log("has money");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if((other.tag == "Bin") || (other.tag == "Chef"))
+        {
+            canClick = true;
+            Debug.Log("can click");
+        }
+    }
+}
